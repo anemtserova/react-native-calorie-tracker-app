1144c6c008b3c934ffc8d5fffbc89c8a8cba0fbb
Antoniya <anemtserova@gmail.com>
2026-01-14 20:00:06 +0200

    add section styles

--- App.js
+++ App.js
@@ -9,21 +9,25 @@ export default function App() {
 				<View style={styles.container}>
 					<StatusBar style="auto" />
 					{/*Header*/}
-					{/* <ScrollView> */}
-					<View>
-						<Text>Calorie Tracker</Text>
+					<View style={styles.infoSection}>
+						<Text style={styles.headerText}>Calorie Tracker</Text>
 					</View>
+					{/* <ScrollView
+                  contentContainerStyle={{
+                    flexGrow: 1,
+                    justifyContent: 'space-between',
+                  }}> */}
 					{/*Overview*/}
-					<View>
-						<Text>Calorie Overview</Text>
+					<View style={styles.infoSection}>
+						<Text style={styles.sectionTitle}>Calorie Overview</Text>
 					</View>
 					{/*List of meals*/}
-					<View>
-						<Text>List of meals</Text>
+					<View style={styles.infoSection}>
+						<Text style={styles.sectionTitle}>List of meals</Text>
 					</View>
 					{/*App Bar*/}
-					<View>
-						<Text>App Bar</Text>
+					<View style={styles.infoSection}>
+						<Text style={styles.sectionTitle}>App Bar</Text>
 					</View>
 					{/* </ScrollView> */}
 				</View>
@@ -32,12 +36,41 @@ export default function App() {
 	);
 }
 
+const colors = {
+	primary: '#f8f8f8',
+	secondary: '#fff',
+	accent: '#e2e2e2',
+};
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		flexDirection: 'column',
-		backgroundColor: '#fff',
+		backgroundColor: colors.primary,
 		alignItems: 'center',
 		justifyContent: 'space-between',
+	},
+	infoSection: {
+		flex: 0.1,
+		width: '80%',
+		backgroundColor: colors.secondary,
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: 10,
+		borderBottomWidth: 1,
+		borderBottomColor: colors.accent,
+		borderRadius: 10,
+		marginTop: 10,
+		boxShadow: '0px 2px 4px rgba(0,0,0,0.1)',
+	},
+	headerText: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+	sectionTitle: {
+		fontSize: 20,
+		fontWeight: '600',
+		marginVertical: 10,
 	},
 });
